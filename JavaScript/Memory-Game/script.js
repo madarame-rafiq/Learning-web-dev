3b6a53c43710a148533a359e1023b28b9cf6224d
Rafiq Mansuri <rafiqmansuri223@gmail.com>
2026-02-28 17:30:00 +0530

    Made the cards in board using position: absolute; and created a class player to manage the red and blue players

--- JavaScript/Memory-Game/script.js
+++ JavaScript/Memory-Game/script.js
@@ -3,16 +3,19 @@
 function generateBoard(){
     let cardsId = 1;
     let board = document.querySelector('.game-board');
+    let top = 10;
     for(let i = 0; i < 5; ++i){
-        let row = document.createElement('div');
-        row.classList.add('row');
+        let left = 10;
         for(let i = 0; i < 4; ++i){
             let card = document.createElement('div');
             card.id = cardsId++;
             card.classList.add('card');
-            row.appendChild(card);
+            card.style.left = `${left}px`;
+            card.style.top = `${top}px`;
+            board.appendChild(card);
+            left += 173;
         }
-        board.appendChild(row);
+        top += 135;
     }
     console.log(board);
 }
@@ -21,14 +24,17 @@ generateBoard();
 
 
 // To generate imaged array. to know what image if where randomly
-const images = Array.from({ length: 10 }, (_, i) => i + 1);
+// Create array with numbers 1–10 appearing twice
+const images = [...Array(10).keys()].map(n => n + 1)  // [1..10]
+  .concat([...Array(10).keys()].map(n => n + 1));  // duplicate
 
-// Shuffle using Fisher-Yates algorithm
+// Shuffle using Fisher–Yates algorithm
 for (let i = images.length - 1; i > 0; i--) {
   const j = Math.floor(Math.random() * (i + 1));
   [images[i], images[j]] = [images[j], images[i]];
 }
-console.log(images)
+
+console.log(images);
 
 
 
@@ -38,11 +44,12 @@ console.log(cards);
 
 // Applying event listener on all the cards 
 cards.forEach(card => {
-    card.addEventListener('click', () => {
+    card.addEventListener('click', (e) => {
+        console.log(e);
         let img = document.createElement('img');
         card.style.transform = `rotateY(90deg)`;
         setTimeout(()=>{
-            img.src = `./assets/img${(card.id == 20 || card.id == 10)?10:card.id%10}.png`;
+            img.src = `./assets/img${(card.id == 20 || card.id == 10)?images[10]:images[card.id%10]}.png`;
             img.width = 90;
             card.appendChild(img);
             card.style.transform = `rotateY(180deg)`;
@@ -58,3 +65,22 @@ cards.forEach(card => {
         },1500);
     });
 });
+
+
+
+
+
+// using Oops to create players i.e, red and blue
+class Player {
+    constructor(color){
+        this.color = color;
+        this.backgroundColor = color;
+        this.score = 0;
+    }
+    changeBackgroundColor(){
+        document.querySelector('.main').style.backgroundColor = `var(--${this.backgroundColor}-bg)`;
+    }
+}
+
+let blue = new Player('blue');
+let red = new Player('red');
